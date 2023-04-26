5d3d722064baaef27f5c10e523d45c2d8ed9799f
Prageeth Peiris <glpspeiris@gmail.com>
2023-04-26 22:30:11 +0530

    display ticket submitted success status

--- app/Http/Livewire/AddTicketForm.php
+++ app/Http/Livewire/AddTicketForm.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Ticket;
+use Awcodes\Shout\Shout;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Contracts\HasForms;
 use Livewire\Component;
@@ -20,6 +21,8 @@ class AddTicketForm extends Component implements HasForms
 
     public $problem;
 
+    public $ref_no;
+
     public function render()
     {
         return view('livewire.add-ticket-form');
@@ -29,6 +32,11 @@ class AddTicketForm extends Component implements HasForms
     protected function getFormSchema(): array
     {
         return [
+            Shout::make('success')
+                ->content(fn() => "Your Ticket was submitted . Your Ticket  Ref No is '{$this->ref_no}' . Please check your ticket status shortly !")
+                ->type('success') // defaults to info
+                ->columnSpan('full')
+            ->hidden(fn() => empty($this->ref_no) ),
             Forms\Components\TextInput::make('customer_name')->label("Your Name")->required(),
             Forms\Components\TextInput::make('customer_email')->label("Your Email")->email()->required(),
             Forms\Components\TextInput::make('customer_phone')->label("Your Phone")->tel()->required(),
@@ -43,7 +51,7 @@ class AddTicketForm extends Component implements HasForms
     public function submit(): void
     {
 
-
+        $ref_no = $this->generateRefNo();
 
         $ticket = new Ticket([
 
@@ -51,18 +59,20 @@ class AddTicketForm extends Component implements HasForms
              'email' => $this->customer_email ,
              'phone' => $this->customer_phone ,
              'problem' => $this->problem ,
-             'ref_no' => $this->generateRefNo()
+             'ref_no' => $ref_no
 
         ]);
 
 
         $ticket->save();
 
+        $this->ref_no = $ref_no;
+
 
     }
 
     private function generateRefNo():string {
-        return  'TICKET-' . time();
+        return  'TICKET-' . $this->customer_email . time();
     }
 
 }
